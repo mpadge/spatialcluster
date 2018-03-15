c1deef0936260e4cd3d625b396ba8b6639d970c5
mpadge <mark.padgham@email.com>
2018-03-15 17:04:39 +0000

    extend src/slk

--- src/slk.cpp
+++ src/slk.cpp
@@ -18,6 +18,48 @@ void contig_mat_init (const Rcpp::DataFrame &gr,
     }
 }
 
+//' merge two clusters in the contiguity matrix, reducing the size of the matrix
+//' by one row and column.
+bool contig_mat_merge (arma::Mat <unsigned short> &contig_mat,
+        int i, int merge_from, int merge_to)
+{
+    bool merged = false;
+    if (contig_mat [i, merge_from] == 1 && // TODO: <- or!
+            contig_mat [merge_from, i] == 1 &&
+            contig_mat [merge_to, i] == 1 &&
+            contig_mat [merge_from, i] == 1)
+    {
+        contig_mat [i, merge_from] = 1;
+        contig_mat [merge_from, i] = 1;
+        contig_mat [merge_to, i] = 1;
+        contig_mat [merge_from, i] = 1;
+        contig_mat.shed_row (merge_from);
+        contig_mat.shed_col (merge_from);
+        merged = true;
+    }
+
+    return merged;
+}
+
+//' does the edge ei from graph_full connect two contiguous clusters?
+bool does_edge_connect (arma::Mat <unsigned short> &contig_mat,
+        uint_map_t edge2cl_map,
+        Rcpp::IntegerVector &from,
+        Rcpp::IntegerVector &to,
+        int ei)
+{
+    //int cl_fr = edge2cl_map.at [from [ei]],
+    //    cl_to = edge2cl_map.at [to [ei]];
+
+    //return (contig_mat [cl_fr, cl_to] == 1);
+    return false;
+}
+
+int find_shortest_connection (uint_map_t edge2cl_map,
+        uint_map_t cl2edge_map)
+{
+}
+
 
 //' rcpp_slk
 //'
@@ -27,9 +69,9 @@ void contig_mat_init (const Rcpp::DataFrame &gr,
 void rcpp_slk (const Rcpp::DataFrame &grfull,
         Rcpp::DataFrame &gr)
 {
-    Rcpp::StringVector from = gr ["from"];
-    Rcpp::StringVector to = gr ["to"];
-    Rcpp::StringVector d = gr ["d"];
+    Rcpp::IntegerVector from = gr ["from"];
+    Rcpp::IntegerVector to = gr ["to"];
+    Rcpp::NumericVector d = gr ["d"];
 
     arma::Mat <unsigned short> contig_mat;
     contig_mat_init (gr, contig_mat);
--- src/slk.h
+++ src/slk.h
@@ -2,8 +2,22 @@
 
 typedef arma::Mat <unsigned short> sint_mat_t;
 
+typedef std::unordered_map <unsigned int, unsigned int> uint_map_t;;
+
 void contig_mat_init (const Rcpp::DataFrame &gr,
         arma::Mat <unsigned short> &contig_mat);
+
+bool contig_mat_merge (arma::Mat <unsigned short> &contig_mat,
+        int i, int merge_from, int merge_to);
+
+bool does_edge_connect (arma::Mat <unsigned short> &contig_mat,
+        uint_map_t edge2cl_map,
+        Rcpp::IntegerVector &from,
+        Rcpp::IntegerVector &to,
+        int ei);
+
+int find_shortest_connection (uint_map_t edge2cl_map,
+        uint_map_t cl2edge_map);
 
 void rcpp_slk (const Rcpp::DataFrame &grfull,
         Rcpp::DataFrame &gr);
