aeea15697769085a228053d3b447b42110e7a2f0
mpadge <mark.padgham@email.com>
2018-03-18 16:26:11 +0000

    extend src/slk

--- src/slk.cpp
+++ src/slk.cpp
@@ -1,26 +1,35 @@
 #include "common.h"
 #include "slk.h"
 
-void contig_mat_init (const Rcpp::DataFrame &gr,
-        arma::Mat <unsigned short> &contig_mat)
+//' initial contiguity and distance matrices. The contiguity matrix is between
+//' clusters, so is constantly modified, whereas the distance matrix is between
+//' edges, so is fixed at load time.
+void mats_init (const Rcpp::DataFrame &gr,
+        arma::Mat <unsigned short> &contig_mat,
+        arma::Mat <double> &d_mat)
 {
     Rcpp::IntegerVector from = gr ["from"];
     Rcpp::IntegerVector to = gr ["to"];
+    Rcpp::NumericVector d = gr ["d"];
 
     const int n = to.length ();
 
     contig_mat = arma::zeros <arma::Mat <unsigned short> > (n, n);
+    d_mat = arma::zeros <arma::Mat <double> > (n, n);
 
     for (int i = 0; i < n; i++)
     {
         contig_mat [from [i], to [i]] = 1;
         contig_mat [to [i], from [i]] = 1;
+        d_mat [from [i], to [i]] = d [i];
+        d_mat [to [i], from [i]] = d [i];
     }
 }
 
 //' merge two clusters in the contiguity matrix, reducing the size of the matrix
 //' by one row and column.
-bool contig_mat_merge (arma::Mat <unsigned short> &contig_mat,
+bool merge_clusters (arma::Mat <unsigned short> &contig_mat,
+        uint_map_t &edge2cl_map, uint_set_map_t &cl2edge_map,
         int i, int merge_from, int merge_to)
 {
     bool merged = false;
@@ -35,6 +44,14 @@ bool contig_mat_merge (arma::Mat <unsigned short> &contig_mat,
         contig_mat [merge_from, i] = 1;
         contig_mat.shed_row (merge_from);
         contig_mat.shed_col (merge_from);
+
+        std::set <unsigned int> edges_i = cl2edge_map.at (merge_from);
+        cl2edge_map.erase (merge_from);
+        std::set <unsigned int> edges_j = cl2edge_map.at (merge_to);
+        for (auto e: edges_i)
+            edges_j.insert (e);
+        cl2edge_map [merge_to] = edges_j;
+
         merged = true;
     }
 
@@ -55,10 +72,28 @@ bool does_edge_connect (arma::Mat <unsigned short> &contig_mat,
     return false;
 }
 
-int find_shortest_connection (uint_map_t edge2cl_map,
-        uint_map_t cl2edge_map)
+int find_shortest_connection (
+        Rcpp::IntegerVector &from,
+        Rcpp::IntegerVector &to,
+        Rcpp::NumericVector &d,
+        uint_map_t edge2cl_map,
+        uint_set_map_t cl2edge_map,
+        int merge_from,
+        int merge_to)
 {
-    int shortest;
+    std::set <unsigned int> edges_i = cl2edge_map.at (merge_from);
+    std::set <unsigned int> edges_j = cl2edge_map.at (merge_to);
+
+    double dmin = INFINITE_DOUBLE;
+    int shortest = INFINITE_INT;
+
+    for (int i = 0; i < from.length (); i++)
+        if (d (from [i], to [i]) < dmin)
+        {
+            dmin = d (from [i], to [i]);
+            shortest = i;
+        }
+       
 
     return shortest;
 }
@@ -77,5 +112,6 @@ void rcpp_slk (const Rcpp::DataFrame &grfull,
     Rcpp::NumericVector d = gr ["d"];
 
     arma::Mat <unsigned short> contig_mat;
-    contig_mat_init (gr, contig_mat);
+    arma::Mat <double> d_mat;
+    mats_init (gr, contig_mat, d_mat);
 }
--- src/slk.h
+++ src/slk.h
@@ -3,11 +3,15 @@
 typedef arma::Mat <unsigned short> sint_mat_t;
 
 typedef std::unordered_map <unsigned int, unsigned int> uint_map_t;;
+typedef std::unordered_map <unsigned int,
+        std::set <unsigned int> > uint_set_map_t;;
 
-void contig_mat_init (const Rcpp::DataFrame &gr,
-        arma::Mat <unsigned short> &contig_mat);
+void mats_init (const Rcpp::DataFrame &gr,
+        arma::Mat <unsigned short> &contig_mat,
+        arma::Mat <double> &d_mat);
 
-bool contig_mat_merge (arma::Mat <unsigned short> &contig_mat,
+bool merge_clusters (arma::Mat <unsigned short> &contig_mat,
+        uint_map_t &edge2cl_map, uint_map_t &cl2edge_map,
         int i, int merge_from, int merge_to);
 
 bool does_edge_connect (arma::Mat <unsigned short> &contig_mat,
@@ -16,8 +20,14 @@ bool does_edge_connect (arma::Mat <unsigned short> &contig_mat,
         Rcpp::IntegerVector &to,
         int ei);
 
-int find_shortest_connection (uint_map_t edge2cl_map,
-        uint_map_t cl2edge_map);
+int find_shortest_connection (
+        Rcpp::IntegerVector &from,
+        Rcpp::IntegerVector &to,
+        Rcpp::NumericVector &d,
+        uint_map_t edge2cl_map,
+        uint_set_map_t cl2edge_map,
+        int merge_from,
+        int merge_to);
 
 void rcpp_slk (const Rcpp::DataFrame &grfull,
         Rcpp::DataFrame &gr);
