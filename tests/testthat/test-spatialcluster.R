2b3df2ff2853e164a1d597ae5a012d2f18f2cbe1
mpadge <mark.padgham@email.com>
2018-05-04 11:57:03 +0200

    :rocket:tests

--- tests/testthat/test-spatialcluster.R
+++ tests/testthat/test-spatialcluster.R
@@ -53,10 +53,11 @@ test_that("plot", {
               xy <- matrix (runif (2 * n), ncol = 2)
               dmat <- matrix (runif (n ^ 2), ncol = n)
               scl <- scl_redcap (xy, dmat, ncl = 4)
-              g <- plot (scl)
-              expect_is (g, "ggplot")
-              g2 <- plot (scl, tree = TRUE)
-              expect_true (!identical (g, g2))
-              g3 <- plot (scl, tree = TRUE, convex = TRUE)
-              expect_true (!identical (g2, g3))
+              g1 <- plot (scl)
+              expect_is (g1, "ggplot")
+              g2 <- plot (scl, convex = FALSE)
+              # The hulls are then contained in
+              h1 <- g1$layers [[2]]$data
+              h2 <- g2$layers [[2]]$data
+              expect_true (!identical (h1, h2))
 })
