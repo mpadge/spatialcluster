05f97de18eb8d099301d7c57c606fc6103ab4e2c
mpadge <mark.padgham@email.com>
2020-12-14 22:04:41 +0100

    test failing example from #22 on gh runners; ping @Nowosad
    I still get no fail, but we'll see what the github runners give for your exact same code ...

---
+++ tests/testthat/test-jakub.R
@@ -0,0 +1,16 @@
+context("issue 22")
+
+test_that("doesthisfail", {
+    set.seed(2020-12-14)
+    n <- 1000
+    xy <- matrix (runif (2 * n), ncol = 2)
+    dmat <- matrix (runif (n ^ 2), ncol = n)
+
+    # works
+    scl <- scl_redcap (xy, dmat, ncl = 8, linkage = "single")
+    expect_is (scl, "scl")
+
+    # R crashes (both inside and outside of RStudio)
+    scl2 <- scl_redcap (xy, dmat, ncl = 50, linkage = "single")
+    expect_is (scl, "scl")
+}
